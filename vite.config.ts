7eaa61d18307ec534101effe0eee9b32aa434f35
Driant Naun <driantn@gmail.com>
2024-11-10 18:08:48 +0100

    added public page action

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "node:path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE,
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
-  },
+    server: {
+      host: "0.0.0.0",
+      port: 3000,
+    },
+  };
 });
